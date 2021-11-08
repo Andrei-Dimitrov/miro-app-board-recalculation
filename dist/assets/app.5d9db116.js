d84ca5d5fbbaf97e903f24f8d46f54a59e048e95
andrei_dmitrov <adimitrov@ebsco.com>
2021-11-08 16:28:30 +0300

    Fix urls

--- dist/assets/app.5d9db116.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */miro.onReady((async function(){const[o]=await miro.board.widgets.create({type:"sticker",text:"Hello, World!"});await miro.board.viewport.zoomToObject(o)}));
