1e25ae01cefdf955a5bc600eadef974e71341bb0
andrei_dmitrov <adimitrov@ebsco.com>
2021-11-24 17:00:20 +0300

    Recalculate board in the interval

--- src/helpers.js
+++ src/helpers.js
@@ -33,21 +33,115 @@ export const updateStatus = async (newStatus) => {
   }
 }
 
-export const handleStickersChange = async (ev) => {
-  console.debug('ev', ev);
-  const itemId = ev.data[0]?.id;
+export const createBoardFrameSelectOptions = async () => {
+  const select = document.getElementById("frame-select");
+  const frames = await miro.board.widgets.get({ type: "frame" });
 
+  frames.forEach((frame) => {
+    select.options[select.options.length] = new Option(frame.title, frame.id);
+  })
+
+  select.addEventListener("change", async(ev) => {
+    const frameId = ev.target.value;
+    window.frame = (await miro.board.widgets.get({ id: frameId }))[0];
+    console.debug('frame', window.frame);
+
+    const appId = miro.getClientId();
+
+    await updateStatus(window.frame?.metadata[appId]?.status ?? "unknown");
+
+    await miro.broadcastData({ frameId, from: "sidebar" });
+  })
+}
+
+export const handleRecalculate = async () => {
   if (!window.frame) {
-    console.error("No frame widget found, unable to update board status :(")
     return;
   }
 
-  const item = itemId ? (await miro.board.widgets.get({ type: "sticker", id: itemId })).filter((item) => withinAllBounds(item, window.frame))[0] : undefined;
+  const table = (await miro.board.widgets.get({ type: "grid"}))
+    .filter(item => withinAllBounds(item, window.frame))[0];
 
-  if (!item) {
-    console.error("No status widget found, unable to update board status :(")
+  const stickers = (await miro.board.widgets.get({ type: "sticker"}))
+    .filter(item => withinAllBounds(item, table));
+
+  const iterations = (await miro.board.widgets.get({ type: "shape"}))
+    .filter(item => withinAllBounds(item, table)).filter((shape) => /vel: \d+\s+ld: \d+/i.test(shape.plainText))
+
+  const features = (await miro.board.widgets.get({ type: "shape"}))
+    .filter(item => withinAllBounds(item, table)).filter((shape) => /size: \d+/i.test(shape.plainText))
+
+  // count iteration loads
+  await Promise.all(iterations.map(async (shape) => {
+    const stickersWithin = stickers.filter((item) => item !== shape && withinYBounds(item, shape));
+
+    const count = countStickersPoints(stickersWithin);
+
+    shape.text = shape.text.replace(/(ld: \d+)/i, `LD: ${count}`);
+
+    console.debug(shape.text);
+
+    await miro.board.widgets.update(shape);
+  }))
+
+  // count feature sizes
+  await Promise.all(features.map(async (shape) => {
+    const stickersWithin = stickers.filter((item) => item !== shape && withinXBounds(item, shape));
+
+    const count = countStickersPoints(stickersWithin);
+
+    shape.text = shape.text.replace(/(size: \d+)/i, `Size: ${count}`);
+
+    console.debug(shape.text);
+
+    await miro.board.widgets.update(shape);
+  }))
+
+  await updateStatus("ok");
+}
+
+export const handleValidate = async () => {
+  if (!window.frame) {
     return;
   }
 
-  await updateStatus("fail");
-};
+  const table = (await miro.board.widgets.get({ type: "grid"}))
+    .filter(item => withinAllBounds(item, window.frame))[0];
+
+  const stickers = (await miro.board.widgets.get({ type: "sticker"}))
+    .filter(item => withinAllBounds(item, table));
+
+  const iterations = (await miro.board.widgets.get({ type: "shape"}))
+    .filter(item => withinAllBounds(item, table)).filter((shape) => /vel: \d+\s+ld: \d+/i.test(shape.plainText))
+
+  const features = (await miro.board.widgets.get({ type: "shape"}))
+    .filter(item => withinAllBounds(item, table)).filter((shape) => /size: \d+/i.test(shape.plainText))
+
+  // count iteration loads
+  const isIterationsValid = iterations.every(async (iteration) => {
+    const stickersWithin = stickers.filter((item) => item !== iteration && withinYBounds(item, iteration));
+
+    const count = countStickersPoints(stickersWithin);
+
+    const iterationCount = Number(iteration.text.match(/ld: (?<count>\d+)/i)?.groups.count ?? 0);
+
+    return count === iterationCount;
+  })
+
+  // count feature sizes
+  const isFeaturesValid = features.every(async (feature) => {
+    const stickersWithin = stickers.filter((item) => item !== feature && withinXBounds(item, feature));
+
+    const count = countStickersPoints(stickersWithin);
+
+    const featureCount = Number(feature.text.match(/size: (?<count>\d+)/i)?.groups.count ?? 0);
+
+    return count === featureCount;
+  })
+
+  if (isIterationsValid && isFeaturesValid) {
+    await updateStatus("ok");
+  } else {
+    await updateStatus("fail");
+  }
+}
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,79 +1,4 @@
-import {
-  countStickersPoints,
-  handleStickersChange,
-  updateStatus,
-  withinAllBounds,
-  withinXBounds,
-  withinYBounds
-} from "./helpers.js";
-
-const createBoardFrameSelectOptions = async () => {
-  const select = document.getElementById("frame-select");
-  const frames = await miro.board.widgets.get({ type: "frame" });
-
-  frames.forEach((frame) => {
-    select.options[select.options.length] = new Option(frame.title, frame.id);
-  })
-
-  select.addEventListener("change", async(ev) => {
-    const frameId = ev.target.value;
-    window.frame = (await miro.board.widgets.get({ id: frameId }))[0];
-    console.debug('frame', window.frame);
-
-    const appId = miro.getClientId();
-
-    await updateStatus(window.frame?.metadata[appId]?.status ?? "unknown");
-
-    await miro.broadcastData({ frameId, from: "sidebar" });
-  })
-}
-
-const handleRecalculate = async () => {
-  if (!window.frame) {
-    return;
-  }
-
-  const table = (await miro.board.widgets.get({ type: "grid"}))
-    .filter(item => withinAllBounds(item, window.frame))[0];
-
-  const stickers = (await miro.board.widgets.get({ type: "sticker"}))
-    .filter(item => withinAllBounds(item, table));
-
-  const iterations = (await miro.board.widgets.get({ type: "shape"}))
-    .filter(item => withinAllBounds(item, table)).filter((shape) => /vel: \d+\s+ld: \d+/i.test(shape.plainText))
-
-
-  const features = (await miro.board.widgets.get({ type: "shape"}))
-    .filter(item => withinAllBounds(item, table)).filter((shape) => /size: \d+/i.test(shape.plainText))
-
-  // count iteration loads
-  await Promise.all(iterations.map(async (shape) => {
-    const stickersWithin = stickers.filter((item) => item !== shape && withinYBounds(item, shape));
-
-    const count = countStickersPoints(stickersWithin);
-
-    shape.text = shape.text.replace(/(ld: \d+)/i, `LD: ${count}`);
-
-    console.debug(shape.text);
-
-    await miro.board.widgets.update(shape);
-  }))
-
-  // count feature sizes
-  await Promise.all(features.map(async (shape) => {
-    const stickersWithin = stickers.filter((item) => item !== shape && withinXBounds(item, shape));
-
-    const count = countStickersPoints(stickersWithin);
-
-    shape.text = shape.text.replace(/(size: \d+)/i, `Size: ${count}`);
-
-    console.debug(shape.text);
-
-    await miro.board.widgets.update(shape);
-  }))
-
-  await updateStatus("ok");
-}
+import { handleRecalculate, createBoardFrameSelectOptions, handleValidate } from "./helpers.js";
 
 miro.onReady(async () => {
   await createBoardFrameSelectOptions();
@@ -82,9 +7,8 @@ miro.onReady(async () => {
 
   recalculateButton.addEventListener("click", handleRecalculate)
 
-  miro.addListener("WIDGETS_CREATED", handleStickersChange)
-  miro.addListener("WIDGETS_DELETED", handleStickersChange)
-  miro.addListener("WIDGETS_TRANSFORMATION_UPDATED", handleStickersChange)
+  setInterval(handleValidate, 1000);
+
   miro.addListener("DATA_BROADCASTED", async (ev) => {
     const frameId = ev.data.frameId;
     if (frameId && ev.data.from === "main") {
